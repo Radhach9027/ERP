59816ac50e59b8c8b46e2b338b16847499e19203
Radhach9027 <Radhachandan9027@gmail.com>
2022-04-25 11:19:09 +0530

    httpMethod, Body Parameter

---
+++ WalmartAssignment/Network/NetworkBodyRequestParameters.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkBodyRequestParameters.swift
+//  WalmartAssignment
+//
+//  Created by radha chilamkurthy on 25/04/22.
+//
+
+typealias NetworkBodyRequestParameters = [String: Any?]
---
+++ WalmartAssignment/Network/NetworkRequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkRequestMethod.swift
+//  WalmartAssignment
+//
+//  Created by radha chilamkurthy on 25/04/22.
+//
+
+import Foundation
+
+enum NetworkRequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
