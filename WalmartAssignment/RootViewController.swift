652f91938b80a32424c3c0f599618daa5539d075
Radhach9027 <Radhachandan9027@gmail.com>
2022-04-25 10:14:51 +0530

    Intial setup with main

---
+++ WalmartAssignment/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartAssignment
+//
+//  Created by radha chilamkurthy on 25/04/22.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+}
+
